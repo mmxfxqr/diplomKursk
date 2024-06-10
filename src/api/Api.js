43720140e627286fe1a00ba56fcb47fcd54d6a2b
mmxfxqr <holodov.sas@yandex.ru>
2024-06-10 20:23:31 +0300

    fedwwqf

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5236/api";
+export const API_URL = "http://localhost:5205/api";
 const $api = axios.create({
   baseURL: API_URL,
 });
