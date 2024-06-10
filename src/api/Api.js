293904124a3066688e1abca0ae9ac4eb1f2b29ae
mmxfxqr <holodov.sas@yandex.ru>
2024-06-10 20:47:08 +0300

    grweqgr

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5205/api";
+export const API_URL = "https://a27673-2ce6.t.d-f.pw/api";
 const $api = axios.create({
   baseURL: API_URL,
 });
